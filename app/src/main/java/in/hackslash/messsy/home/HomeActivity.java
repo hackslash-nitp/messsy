dfbfd29eb3f1b859690c9e00d7c2870b1e4e33bf
mdzaidalam52 <mdzaidalam001@gmail.com>
2020-05-17 11:29:24 +0530

    Added Recycler View for upcoming meals to the Home UI

--- app/src/main/java/in/hackslash/messsy/home/HomeActivity.java
+++ app/src/main/java/in/hackslash/messsy/home/HomeActivity.java
@@ -2,16 +2,48 @@ package in.hackslash.messsy.home;
 
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import in.hackslash.messsy.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    RecyclerView upcomingRecycler;
+    ArrayList<CardViewModel> model;
+    UpcomingRecycler adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        upcomingRecycler = (RecyclerView)findViewById(R.id.upcoming_meals);
+
+        int[] image = {R.drawable.boiled_eggs, R.drawable.bread, R.drawable.milk};
+        String[] name = {"Boled eggs", "Bread", "Milk"};
+
+        model = new ArrayList<>();
+        for(int i = 0; i<image.length; i++){
+            CardViewModel model1 = new CardViewModel(image[i], name[i]);
+            model.add(model1);
+        }
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                HomeActivity.this, LinearLayoutManager.HORIZONTAL, false
+        );
+        upcomingRecycler.setLayoutManager(layoutManager);
+        upcomingRecycler.setItemAnimator(new DefaultItemAnimator());
+
+        adapter = new UpcomingRecycler(model, HomeActivity.this);
+
+        upcomingRecycler.setAdapter(adapter);
     }
 }
--- app/src/main/java/in/hackslash/messsy/home/UpcomingRecycler.java
+++ app/src/main/java/in/hackslash/messsy/home/UpcomingRecycler.java
@@ -1,34 +1,56 @@
 package in.hackslash.messsy.home;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import in.hackslash.messsy.R;
+
 public class UpcomingRecycler extends RecyclerView.Adapter<UpcomingRecycler.ViewHolder> {
+
+    ArrayList<CardViewModel> model;
+    Context mContext;
+
+    public UpcomingRecycler(ArrayList<CardViewModel> model, Context context) {
+        this.model = model;
+        mContext = context;
+    }
 
     // Use upcoming_meal.xml to define the layout of single item of the list and remove this comment when don
     @NonNull
     @Override
     public UpcomingRecycler.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_upcoming_meal_card, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.mImageView.setImageResource(model.get(position).getFoodImage());
+        holder.mTextView.setText(model.get(position).getNameFood());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return model.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-
+        ImageView mImageView;
+        TextView mTextView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mImageView = itemView.findViewById(R.id.upcomingcard_pic);
+            mTextView = itemView.findViewById(R.id.upcomingcard_name);
         }
     }
 }
