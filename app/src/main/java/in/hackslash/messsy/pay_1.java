aa66604b26e168d8e0a77da5d43fd598b8bc1dde
priyanshu <priyanshubashu7421@gmail.com>
2020-05-25 22:21:57 +0530

    Completed pay_ui

---
+++ app/src/main/java/in/hackslash/messsy/pay_1.java
@@ -0,0 +1,14 @@
+package in.hackslash.messsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pay_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_1);
+    }
+}
