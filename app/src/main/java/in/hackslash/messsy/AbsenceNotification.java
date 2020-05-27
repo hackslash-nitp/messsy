028814010d4f183355fd565054826fa837f3199e
rishi <rishitiwari517@gmail.com>
2020-05-27 20:48:31 +0530

    Absence notification UI

---
+++ app/src/main/java/in/hackslash/messsy/AbsenceNotification.java
@@ -0,0 +1,14 @@
+package in.hackslash.messsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbsenceNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absence_notification);
+    }
+}
