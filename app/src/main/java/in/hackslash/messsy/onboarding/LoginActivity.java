5f6ab23d62ade7d8ca78a8d8091ed888ffade8da
rishi <rishitiwari517@gmail.com>
2020-06-07 17:44:32 +0530

    Login Activity and User Modal class

--- app/src/main/java/in/hackslash/messsy/onboarding/LoginActivity.java
+++ app/src/main/java/in/hackslash/messsy/onboarding/LoginActivity.java
@@ -1,13 +1,26 @@
 package in.hackslash.messsy.onboarding;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import in.hackslash.messsy.R;
+import in.hackslash.messsy.home.HomeActivity;
 
 public class LoginActivity extends AppCompatActivity {
     // TODO Declare instances for all UI elements
+    private EditText email_et, password_et;
+    private Button login;
+    private TextView create_account, forgot_pass;
+    private AccountsUtil accountsUtil;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,13 @@ public class LoginActivity extends AppCompatActivity {
 
     public void assignVariables() {
         // TODO initialize all fields here
+        email_et = findViewById(R.id.line_1);
+        password_et = findViewById(R.id.line_2);
+        login = findViewById(R.id.rectangle_1);
+        create_account = findViewById(R.id.create_account);
+        forgot_pass = findViewById(R.id.forgot_password);
+        progressBar=findViewById(R.id.login_progress);
+        accountsUtil = new AccountsUtil();
     }
 
     public void setup() {
@@ -31,20 +51,77 @@ public class LoginActivity extends AppCompatActivity {
 
     private void handleCreateAccount() {
         // TODO define onclicklistener on Create Account button to navigate to CreateAccountActivity and close this activity
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void handleForgotPassword() {
         // TODO Later
+        forgot_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    public void handleLogin(){
-        // TODO define onclicklistener on Sign In button to collect data from all edittexts and call ComplaintsUtil.loginUser
+    public void handleLogin() {
 
-        // TODO Show an indefinite progress bar on the screen as soon as the submit button is clicked to denote that data is being processed
+        //  define onclicklistener on Sign In button to collect data from all edittexts and call AccountUtil.loginUser
 
-        // TODO Pass a callback by loginUser().setOnCompleteListener() to handle the result from the update operation. Show a toast according to the result and also hide the progress bar.
 
-        // TODO NOTE: If the result is SUCCESS, then close this activity and navigate to HomeActivity
+        //  Show an indefinite progress bar on the screen as soon as the submit button is clicked to denote that data is being processed
+
+
+        //  Pass a callback by loginUser().setOnCompleteListener() to handle the result from the update operation. Show a toast according to the result and also hide the progress bar.
+
+
+        //  NOTE: If the result is SUCCESS, then close this activity and navigate to HomeActivity
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = email_et.getText().toString();
+                String password = password_et.getText().toString();
+                User user = new User();
+                user.setEmail(email);
+                accountsUtil.loginUser(user, password).setOnCompleteListener(new AccountsUtil.onCompleteListener() {
+                    @Override
+                    public void onComplete(AccountsUtil.Status status, User user) {
+                        if (status == AccountsUtil.Status.ALREADY_LOGGED_IN) {
+                            //user already logged in
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(LoginActivity.this, "Already Logged in", Toast.LENGTH_SHORT).show();
+                        } else if (status == AccountsUtil.Status.WRONG_INPUT) {
+                            //wrong input
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(LoginActivity.this, "wrong input", Toast.LENGTH_SHORT).show();
+                        } else if (status == AccountsUtil.Status.DUPLICATE_USER) {
+                            //user already exist
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(LoginActivity.this, "user with this email already exist", Toast.LENGTH_SHORT).show();
+                        } else if (status == AccountsUtil.Status.SUCCESS) {
+                            //logged in successful
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(LoginActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+
+            }
+        });
+
 
     }
+
 }
--- app/src/main/java/in/hackslash/messsy/onboarding/User.java
+++ app/src/main/java/in/hackslash/messsy/onboarding/User.java
@@ -3,4 +3,38 @@ package in.hackslash.messsy.onboarding;
 public class User {
 
     // TODO add user fields here
+    private String email,name,roomno;
+
+    public User(){
+
+    }
+    public User(String email, String name, String roomno) {
+        this.email = email;
+        this.name = name;
+        this.roomno = roomno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoomno() {
+        return roomno;
+    }
+
+    public void setRoomno(String roomno) {
+        this.roomno = roomno;
+    }
 }
