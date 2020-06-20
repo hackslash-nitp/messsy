a67a793ee27c0edcd0c970d2cd0d2ffcb65c9d1a
Rishikant Tiwari <rishitiwari517@gmail.com>
2020-06-21 00:00:57 +0530

    Added AccountsUtil

--- app/src/main/java/in/hackslash/messsy/onboarding/User.java
+++ app/src/main/java/in/hackslash/messsy/onboarding/User.java
@@ -3,16 +3,25 @@ package in.hackslash.messsy.onboarding;
 public class User {
 
     // TODO add user fields here
-    private String email,name,roomno;
+    private String email,name,roomno,uid;
 
     public User(){
 
     }
-    public User(String email, String name, String roomno) {
+
+    public User(String email, String name, String roomno,String uid) {
         this.email = email;
         this.name = name;
         this.roomno = roomno;
+        this.uid=uid;
     }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
 
     public String getEmail() {
         return email;
