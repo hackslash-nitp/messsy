72fe13003fe8f9b4c3e8ea5913dfe3227f9f2761
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-01-07 23:47:47 +0530

    linked ComplaintActivity,AbsenceNotification and NoticeActivity with corresponding FireStore Collection and added recycler view to NoticeActivity for notifications

--- app/src/main/java/in/hackslash/messsy/AbsenceNotification.java
+++ app/src/main/java/in/hackslash/messsy/AbsenceNotification.java
@@ -3,12 +3,60 @@ package in.hackslash.messsy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.print.PrinterId;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AbsenceNotification extends AppCompatActivity {
+
+    private final String START_DATE="START DATE";
+    private final String END_DATE="END DATE";
+    private final String MISSED_MEAL="MISSING MEAL";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference mealRef=db.collection("AbsenceNotification");
+
+    EditText startDate;
+    EditText endDate;
+    Button okbtn,cancelbtn;
+    RadioGroup rg;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absence_notification);
+        startDate = findViewById(R.id.start_date_input);
+        endDate = findViewById(R.id.end_date_input);
+        okbtn = findViewById(R.id.OK);
+        cancelbtn = findViewById(R.id.cancel);
+        rg=findViewById(R.id.radio_buttom_group_a);
+        okbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upload(v);
+            }
+        });
+
+    }
+    public void upload(View v){
+        Map<String,Object> meal=new HashMap<>();
+        String missedMeal=((RadioButton)findViewById(rg.getCheckedRadioButtonId())).getText().toString();
+        String Start_date= startDate.getText().toString();
+        String end_date=endDate.getText().toString();
+        meal.put(START_DATE,Start_date);
+        meal.put(END_DATE,end_date);
+        meal.put(MISSED_MEAL,missedMeal);
+        mealRef.add(meal);
     }
 }
--- app/src/main/java/in/hackslash/messsy/complaint/ComplaintActivity.java
+++ app/src/main/java/in/hackslash/messsy/complaint/ComplaintActivity.java
@@ -1,33 +1,123 @@
 package in.hackslash.messsy.complaint;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import in.hackslash.messsy.R;
 
 public class ComplaintActivity extends AppCompatActivity {
 
     // TODO Declare instances for all UI elements
+    EditText issue;
+    EditText des;
+    ImageView imgView;
+    private static final int IMAGE_CODE=1;
+    Button upload,submit;
+    Uri downloadURL;
+    StorageReference mstorageReference;
+    FirebaseFirestore dbs = FirebaseFirestore.getInstance();
+    private final CollectionReference complaintRef = dbs.collection("ComplaintSection");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+        issue=findViewById(R.id.line_1);
+        des=findViewById(R.id.line_2);
+        upload=findViewById(R.id.upload_button);
+        submit=findViewById(R.id.submit_button);
+        mstorageReference= FirebaseStorage.getInstance().getReference().child("images");
 
-        assignVariables();
+
         setup();
     }
 
-    public void assignVariables() {
-        // TODO initialize all fields here
-    }
+
 
     public void setup() {
         // TODO define onclicklistener on File Complaints button to collect data from all edittexts and call ComplaintsUtil.createComplaint
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            f(v);
+
+            }
+        });
         // TODO Show an indefinite progress bar on the screen as soon as the submit button is clicked to denote that data is being processed
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent().setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Complete action using"), IMAGE_CODE);
 
+
+            }
+        });
         // TODO Pass a callback by createComplaint().setOnCompleteListener() to handle the result from the create operation. Show a toast according to the result and also hide the progress bar.
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==IMAGE_CODE&& resultCode==RESULT_OK){
+            assert data != null;
+            Uri selected=data.getData();
+            try {
+                Bitmap currentImage = MediaStore.Images.Media.getBitmap
+                        (this.getContentResolver(), selected);
+
+
+                imgView.setImageBitmap(currentImage);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+                StorageReference photoRef = mstorageReference.child(selected.getLastPathSegment());
+            Toast.makeText(ComplaintActivity.this,"Loading",Toast.LENGTH_SHORT).show();
+
+            photoRef.putFile(selected).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful());
+                     downloadURL = urlTask.getResult();
+
+
+
+                }
+
+            });
+        }
+    }
+    public void f(View v){
+        String s = String.valueOf(downloadURL);
+        String s1=issue.getText().toString();
+        String s2=des.getText().toString();
+        Complaint complaint=new Complaint(s1,s2,s);
+        complaintRef.add(complaint);
+
     }
 }
