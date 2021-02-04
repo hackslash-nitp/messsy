b2b5afe2eedd81fd3290fc864b14025ee1505c61
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-02-04 15:41:19 +0530

    fragment modified

--- app/src/main/java/in/hackslash/messsy/home/QRFragment.java
+++ app/src/main/java/in/hackslash/messsy/home/QRFragment.java
@@ -1,12 +1,35 @@
 package in.hackslash.messsy.home;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.type.DateTime;
+import com.google.zxing.WriterException;
+
+import java.io.ByteArrayOutputStream;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 import in.hackslash.messsy.R;
 
 /**
@@ -20,6 +43,13 @@ public class QRFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Long time;
+    Button b1;
+    StorageReference QRstorageReference;
+    String date;
+    ImageView img;
+    TextView txt;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,6 +90,49 @@ public class QRFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_q_r, container, false);
+        View rootView=inflater.inflate(R.layout.fragment_q_r, container, false);
+        b1=rootView.findViewById(R.id.generate);
+        txt=rootView.findViewById(R.id.nameID);
+        img=rootView.findViewById(R.id.imageViewQR);
+        QRstorageReference= FirebaseStorage.getInstance().getReference().child("QRcode");
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gen();
+            }
+        });
+        return rootView;
+    }
+    private void gen(){
+        final String data=txt.getText().toString();
+        if(img.getDrawable()==null) {
+            QRGEncoder qrgEncoder = new QRGEncoder(data, null, QRGContents.Type.TEXT, 200);
+            Bitmap QRBits = qrgEncoder.getBitmap();
+            img.setImageBitmap(QRBits);
+
+            Uri uri=getImageUri(getContext(),QRBits);
+            Log.d("abcd","Uri is"+uri);
+//            StorageReference photoRef = QRstorageReference.child(uri.getLastPathSegment());
+
+//            photoRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    Toast.makeText(getActivity(), "QR uploaded", Toast.LENGTH_SHORT).show();
+//
+//                }
+//            });
+
+        }
+        else {
+
+            Toast.makeText(getActivity(), "QR already generated"+time, Toast.LENGTH_SHORT).show();
+        }
+    }
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 }
