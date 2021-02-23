5c891f7721f375312e36fd3e11ff06a7dd344356
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-02-23 15:09:26 +0530

    QR fragment updated

--- app/src/main/java/in/hackslash/messsy/complaint/NoticeActivity.java
+++ app/src/main/java/in/hackslash/messsy/complaint/NoticeActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -21,18 +24,21 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+import in.hackslash.messsy.AbsenceNotification;
 import in.hackslash.messsy.R;
+import in.hackslash.messsy.home.HomeActivity;
 
 public class NoticeActivity extends AppCompatActivity {
 
     // TODO Declare instances for all UI elements
 
     RecyclerView r;
-    private final String TAG="abcd";
-    private final FirebaseFirestore db=FirebaseFirestore.getInstance();
+    ImageView imageView;
+    private final String TAG = "abcd";
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final CollectionReference notebookRef = db.collection("MessssyNotice");
     Notice madapter = new Notice(this);
-    ArrayList<String> notice=new ArrayList<>();
+    ArrayList<String> notice = new ArrayList<>();
 
 
     @Override
@@ -40,35 +46,40 @@ public class NoticeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
         r = findViewById(R.id.recycler_view);
+        imageView=findViewById(R.id.arrow_1);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NoticeActivity.this, HomeActivity.class));
+            }
+        });
         RecyclerView.LayoutManager layoutManager;
         layoutManager = new LinearLayoutManager(this);
         r.setLayoutManager(layoutManager);
-load();
-r.setAdapter(madapter);
+        load();
+        r.setAdapter(madapter);
+
     }
 
 
-
-
-
-    public void load(){
+    public void load() {
         notebookRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
-                    String note = documentSnapshot.toObject(String.class);
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    String note = documentSnapshot.getString("notice");
 
                     notice.add(note);
                 }
+                madapter.updatefood(notice);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.d(TAG,e.toString());
+                Log.d(TAG, e.toString());
 
             }
         });
-        madapter.updatefood(notice);
     }
 }
--- app/src/main/java/in/hackslash/messsy/vote/VoteActivity.java
+++ app/src/main/java/in/hackslash/messsy/vote/VoteActivity.java
@@ -2,20 +2,32 @@ package in.hackslash.messsy.vote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import in.hackslash.messsy.AbsenceNotification;
 import in.hackslash.messsy.R;
+import in.hackslash.messsy.home.HomeActivity;
 
 public class VoteActivity extends AppCompatActivity {
 
     // TODO Declare instances for all UI elements
-
+ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
         assignVariables();
         setup();
+        imageView=findViewById(R.id.arrow_3);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(VoteActivity.this, HomeActivity.class));
+            }
+        });
     }
 
     public void assignVariables() {
