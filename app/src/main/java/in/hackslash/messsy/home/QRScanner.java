387c5177856fe2734e10e3fda319f671d5e76881
Amit <amitk.ug19.me@nitp.ac.in>
2021-05-30 19:52:39 +0530

    Added FloatingActionButton for QRSCanner

---
+++ app/src/main/java/in/hackslash/messsy/home/QRScanner.java
@@ -0,0 +1,16 @@
+package in.hackslash.messsy.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.hackslash.messsy.R;
+
+public class QRScanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscanner);
+    }
+}
