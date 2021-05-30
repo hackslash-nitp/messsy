ed081f1ecaaa577020cf4c6c3e5395404e22a51d
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-05-31 00:26:00 +0530

    added qrscan

--- app/src/main/java/in/hackslash/messsy/home/QRFragment.java
+++ app/src/main/java/in/hackslash/messsy/home/QRFragment.java
@@ -1,44 +1,23 @@
 package in.hackslash.messsy.home;
 
-import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.google.type.DateTime;
-import com.google.zxing.WriterException;
-
-import java.io.ByteArrayOutputStream;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
-
 import in.hackslash.messsy.R;
 
 public class QRFragment extends Fragment {
@@ -53,7 +32,6 @@ public class QRFragment extends Fragment {
     String date;
     ImageView img;
     TextView txt;
-
 
 
     // TODO: Rename and change types of parameters
@@ -95,20 +73,21 @@ public class QRFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView=inflater.inflate(R.layout.fragment_q_r, container, false);
-        txt=rootView.findViewById(R.id.nameID);
+        View rootView = inflater.inflate(R.layout.fragment_q_r, container, false);
+        txt = rootView.findViewById(R.id.nameID);
         txt.setText(HomeFragment.userName);
-        img=rootView.findViewById(R.id.imageViewQR);
-        QRstorageReference= FirebaseStorage.getInstance().getReference().child("QRcode");
+        img = rootView.findViewById(R.id.imageViewQR);
+        QRstorageReference = FirebaseStorage.getInstance().getReference().child("QRcode");
         gen();
 
         return rootView;
     }
-    private void gen(){
-        final String data=txt.getText().toString();
+
+    private void gen() {
+        final String data = txt.getText().toString();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String email = user.getEmail();
-        if(img.getDrawable()==null) {
+        if (img.getDrawable() == null) {
             QRGEncoder qrgEncoder = new QRGEncoder(email, null, QRGContents.Type.TEXT, 300);
             Bitmap QRBits = qrgEncoder.getBitmap();
             img.setImageBitmap(QRBits);
@@ -118,11 +97,11 @@ public class QRFragment extends Fragment {
         }
 
     }
-    }
+}
 //    public Uri getImageUri(Context inContext, Bitmap inImage) {
 //        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 //        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
 //        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
 //        return Uri.parse(path);
 //    }
-}
+
