d9542e6c92b60dc631ef67cb234b402175cc4f76
SAMEER SHEKHAR <64265450+sameershekhar100@users.noreply.github.com>
2021-05-31 02:19:43 +0530

    added qr scanner (#62)
    * added qrscan
    
    Co-authored-by: MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
    Co-authored-by: Ayushmaan sharma <55322319+ayushman999@users.noreply.github.com>

--- app/src/main/java/in/hackslash/messsy/home/QRFragment.java
+++ app/src/main/java/in/hackslash/messsy/home/QRFragment.java
@@ -1,72 +1,107 @@
 package in.hackslash.messsy.home;
 
-import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.google.type.DateTime;
-import com.google.zxing.WriterException;
-
-import java.io.ByteArrayOutputStream;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
-
 import in.hackslash.messsy.R;
 
 public class QRFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    Long time;
+    Button b1;
     StorageReference QRstorageReference;
+    String date;
     ImageView img;
     TextView txt;
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public QRFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment QRFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static QRFragment newInstance(String param1, String param2) {
+        QRFragment fragment = new QRFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView=inflater.inflate(R.layout.fragment_q_r, container, false);
-        txt=rootView.findViewById(R.id.nameID);
+        View rootView = inflater.inflate(R.layout.fragment_q_r, container, false);
+        txt = rootView.findViewById(R.id.nameID);
         txt.setText(HomeFragment.userName);
-        img=rootView.findViewById(R.id.imageViewQR);
-        QRstorageReference= FirebaseStorage.getInstance().getReference().child("QRcode");
+        img = rootView.findViewById(R.id.imageViewQR);
+        QRstorageReference = FirebaseStorage.getInstance().getReference().child("QRcode");
         gen();
 
         return rootView;
     }
-    private void gen(){
+
+    private void gen() {
+        final String data = txt.getText().toString();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String email = user.getEmail();
-        if(img.getDrawable()==null) {
+        if (img.getDrawable() == null) {
             QRGEncoder qrgEncoder = new QRGEncoder(email, null, QRGContents.Type.TEXT, 300);
             Bitmap QRBits = qrgEncoder.getBitmap();
             img.setImageBitmap(QRBits);
+//
+//
+
         }
 
     }
 }
+//    public Uri getImageUri(Context inContext, Bitmap inImage) {
+//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+//        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+//        return Uri.parse(path);
+//    }
+
