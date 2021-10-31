13f52a23aeeeb8ef90d8c7e97142c45a7a18b864
Tanvi Tanya <tanvitanya15@gmail.com>
2021-10-31 23:27:53 +0530

    Setup Notice Activity

--- app/src/main/java/in/hackslash/messsy/complaint/NoticeActivity.java
+++ app/src/main/java/in/hackslash/messsy/complaint/NoticeActivity.java
@@ -1,7 +1,6 @@
 package in.hackslash.messsy.complaint;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,16 +14,12 @@ import android.widget.ImageView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-import in.hackslash.messsy.AbsenceNotification;
 import in.hackslash.messsy.R;
 import in.hackslash.messsy.home.HomeActivity;
 
@@ -34,11 +29,15 @@ public class NoticeActivity extends AppCompatActivity {
 
     RecyclerView r;
     ImageView imageView;
+
+    String date, description, subject, time, user, hostel, designation;
+
+
     private final String TAG = "abcd";
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final CollectionReference notebookRef = db.collection("MessssyNotice");
     Notice madapter = new Notice(this);
-    ArrayList<String> notice = new ArrayList<>();
+    ArrayList<NoticeBoxAdapterClass> notice = new ArrayList<NoticeBoxAdapterClass>();
 
 
     @Override
@@ -54,7 +53,7 @@ public class NoticeActivity extends AppCompatActivity {
             }
         });
         RecyclerView.LayoutManager layoutManager;
-        layoutManager = new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         r.setLayoutManager(layoutManager);
         load();
         r.setAdapter(madapter);
@@ -68,9 +67,15 @@ public class NoticeActivity extends AppCompatActivity {
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
                 for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                    String note = documentSnapshot.getString("notice");
+                    date = documentSnapshot.getString("date");
+                    description = documentSnapshot.getString("description");
+                    subject = documentSnapshot.getString("subject");
+                    time = documentSnapshot.getString("timestamp");
+                    user = documentSnapshot.getString("userInfo");
+                    hostel = documentSnapshot.getString("hostel");
+                    designation = documentSnapshot.getString("designation");
 
-                    notice.add(note);
+                    notice.add(new NoticeBoxAdapterClass(subject, description, date, time, designation, hostel));
                 }
                 madapter.updatefood(notice);
             }
