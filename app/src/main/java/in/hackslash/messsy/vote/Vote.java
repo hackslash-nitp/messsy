de44ac5da3b34e28461ec6b2c7617935aaea59ca
Tanvi Tanya <85440564+tanvitanyaa@users.noreply.github.com>
2021-11-21 16:35:26 +0530

    Setup Vote for Menu Change (#74)
    * Added OnBoarding Activity
    
    * Fixed Back Button issue
    
    * Updated AbsenceNotification.java
    
    * Setup Notice Activity
    
    * Setup VoteForMenuChange Activity

--- app/src/main/java/in/hackslash/messsy/vote/Vote.java
+++ app/src/main/java/in/hackslash/messsy/vote/Vote.java
@@ -2,5 +2,38 @@ package in.hackslash.messsy.vote;
 
 public class Vote {
 
-    // TODO Define vote fields here
+    String mealTime, foodItem, addOrRemove;
+
+    Vote() {}
+
+    public Vote(String sMealTime, String sFoodItem, String sAddOrRemove) {
+        this.mealTime = sMealTime;
+        this.foodItem = sFoodItem;
+        this.addOrRemove = sAddOrRemove;
+    }
+
+    public String getMealTime() {
+        return mealTime;
+    }
+
+    public void setMealTime(String mealTime) {
+        this.mealTime = mealTime;
+    }
+
+    public String getFoodItem() {
+        return foodItem;
+    }
+
+    public void setFoodItem(String foodItem) {
+        this.foodItem = foodItem;
+    }
+
+    public String getAddOrRemove() {
+        return addOrRemove;
+    }
+
+    public void setAddOrRemove(String addOrRemove) {
+        this.addOrRemove = addOrRemove;
+    }
+
 }
--- app/src/main/java/in/hackslash/messsy/vote/VoteActivity.java
+++ app/src/main/java/in/hackslash/messsy/vote/VoteActivity.java
@@ -1,11 +1,27 @@
 package in.hackslash.messsy.vote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import in.hackslash.messsy.AbsenceNotification;
 import in.hackslash.messsy.R;
@@ -14,7 +30,31 @@ import in.hackslash.messsy.home.HomeActivity;
 public class VoteActivity extends AppCompatActivity {
 
     // TODO Declare instances for all UI elements
-ImageView imageView;
+    ImageView imageView;
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
+    EditText FoodItem;
+
+    String addOrRemove = "add";
+
+
+
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    DocumentReference documentReference;
+    FirebaseAuth firebaseAuth ;
+    FirebaseUser currentUser;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    Bitmap bitmap;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +66,75 @@ ImageView imageView;
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(VoteActivity.this, HomeActivity.class));
+            }
+        });
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
+        documentReference = FirebaseFirestore.getInstance().collection("Menu Change").document();
+
+
+        radioGroup = findViewById(R.id.radio_buttom_group);
+        FoodItem = findViewById(R.id.enter_food_item);
+
+        Button add = findViewById(R.id.vote_for_ad);
+        Button remove = findViewById(R.id.vote_for_re);
+
+        Button submit = findViewById(R.id.submit_button);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addOrRemove = "add";
+            }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addOrRemove = "remove";
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String food = FoodItem.getText().toString();
+
+                if(food.isEmpty())
+                {
+                    FoodItem.setError("Specify Food Item.");
+                    return;
+                }
+
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+
+                String foodTime = radioButton.getText().toString();
+
+                Vote voteForChange = new Vote(foodTime, food, addOrRemove);
+
+
+                firebaseFirestore.collection("Menu Change").document().set(voteForChange).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        Toast.makeText(VoteActivity.this, "Request sent to " + addOrRemove + " " + food + " in " + foodTime + ".", Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(VoteActivity.this, "Request Failed.", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+
+
             }
         });
     }
